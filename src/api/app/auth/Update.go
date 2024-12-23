fbf320bf8e828f1a916ca1ca77f9775e7e11a9c9
guozejia <guojia99@foxmail.com>
2024-12-23 13:58:21 +0800

    [20241223] 修改

--- src/api/app/auth/Update.go
+++ src/api/app/auth/Update.go
@@ -1,12 +1,13 @@
 package auth
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/guojia99/cubing-pro/src/api/exception"
 	"github.com/guojia99/cubing-pro/src/api/middleware"
+	"github.com/guojia99/cubing-pro/src/internel/database/model/competition"
+	"github.com/guojia99/cubing-pro/src/internel/database/model/result"
 	"github.com/guojia99/cubing-pro/src/internel/svc"
 	"github.com/guojia99/cubing-pro/src/internel/utils"
 )
@@ -32,18 +33,34 @@ func UpdateDetail(svc *svc.Svc) gin.HandlerFunc {
 			exception.ErrRequestBinding.ResponseWithError(ctx, err)
 			return
 		}
+
+		// todo 添加事务，可回滚
+
+		var updateName = req.Name != user.Name
+
+		if updateName && user.LastUpdateNameTime != nil && time.Since(*user.LastUpdateNameTime) < time.Minute*30 {
+			exception.ErrAuthField.ResponseWithError(ctx, "30分钟内无法重复改名，请等待耐心等待")
+			return
+		}
+
+		if updateName {
+			svc.DB.Model(&result.Results{}).Where("cube_id = ?", user.CubeID).Update("person_name", req.Name)
+			svc.DB.Model(&competition.Registration{}).Where("user_id = ?", user.ID).Update("user_name", req.Name)
+			user.LastUpdateNameTime = utils.PtrNow()
+		}
+
 		user.Name = req.Name
 		user.EnName = req.EnName
 		user.WcaID = req.WcaID
 		user.QQ = req.QQ
 		user.Sign = req.Sign
 		user.Sex = req.Sex
-		fmt.Println(req.Sex)
 
 		if req.Birthdate != "" {
 			b, _ := time.Parse("2006-01-02", req.Birthdate)
 			user.Birthdate = utils.PtrTime(b)
 		}
 		svc.DB.Save(&user)
+
 	}
 }

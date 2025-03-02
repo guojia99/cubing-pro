60e40f2ba57e2ffb871aec954ebebe6c507a06a4
guozejia <guojia99@foxmail.com>
2025-03-02 12:10:42 +0800

    [2025] 修改爬虫报告

--- src/internel/convenient/job/jj_crawler.go
+++ src/internel/convenient/job/jj_crawler.go
@@ -2,6 +2,7 @@ package job
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/guojia99/cubing-pro/src/internel/configs"
 	"github.com/guojia99/cubing-pro/src/internel/email"
@@ -19,13 +20,15 @@ func (c *JJCrawler) Name() string {
 func (c *JJCrawler) Run() error {
 	find := crawler.CheckAllCompetition()
 
+	log.Printf("cubing获取开始")
 	if len(find) >= 0 {
-		msg := "获取到链接为:"
+		msg := "获取到链接为:\n"
 		for _, v := range find {
 			msg += fmt.Sprintf("%s\n", v)
 		}
 		err := email.SendEmail(c.Config.GlobalConfig.EmailConfig, "爬虫报告", []string{"guojia99@foxmail.com"}, []byte(msg))
 		return err
 	}
+	log.Printf("cubing获取结束")
 	return nil
 }
